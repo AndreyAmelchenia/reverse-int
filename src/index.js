4c903e686796cd99f44c8ea03ad5e940148493d5
AndreyAmelchenia <60568794+AndreyAmelchenia@users.noreply.github.com>
2020-03-20 12:09:43 +0300

    docs: Ok

--- src/index.js
+++ src/index.js
@@ -1,3 +1,14 @@
 module.exports = function reverse (n) {
-  
+    let x = 0;
+       let m = 0; 
+    while(0 != n){
+        m = n%10; 
+        n = (n-n%10)/10;
+        x = x*10+m;       
+    }
+    if (x<0){
+        x=x*-1;
+    }
+   
+    return x
 }
